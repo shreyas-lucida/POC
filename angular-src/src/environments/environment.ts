2d586b8519223a79be80784b1c7c3267a9c03ed7
Shreyas <shreyas.kumar.official@gmail.com>
2020-10-24 12:47:03 +0530

    env change

--- angular-src/src/environments/environment.ts
+++ angular-src/src/environments/environment.ts
@@ -9,9 +9,9 @@ export const environment = {
     'facebook': '223045385190067',
     'google': '1086867360709-69ko7vlgcc3uuq8a42dmmvgjng1vg02l.apps.googleusercontent.com'
   },
-  clientId: '8c07b072-769b-4644-bc93-396ea8f60dda',
+  clientId: 'af23612e-ff6b-4828-9293-0b2542fff338',
   authority: 'https://login.microsoftonline.com/af23612e-ff6b-4828-9293-0b2542fff338/',
-  redirectUrl: 'https://alg.netlify.app/home'
+  redirectUrl: 'https://cm3dapp.azurewebsites.net/home'
 };
 
 /*
