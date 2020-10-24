277d29b091d14a0548bac044d3385bce8a7db01a
Shreyas <shreyas.kumar.official@gmail.com>
2020-10-24 13:53:00 +0530

    env change

--- angular-src/src/environments/environment.prod.ts
+++ angular-src/src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
-  production: true,
+  production: false,
   apiServer: 'http://localhost:3000',
   socialLogin: {
     'facebook': '223045385190067',
     'google': '1086867360709-69ko7vlgcc3uuq8a42dmmvgjng1vg02l.apps.googleusercontent.com'
-  }
+  },
+  clientId: 'af23612e-ff6b-4828-9293-0b2542fff338',
+  authority: 'https://login.microsoftonline.com/af23612e-ff6b-4828-9293-0b2542fff338/',
+  redirectUrl: 'https://cm3dapp.azurewebsites.net/home'
 };
